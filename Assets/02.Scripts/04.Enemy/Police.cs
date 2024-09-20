139a8f19903fd5d5d68d1b97f7b388a2ca4ad8b5
Lee Bohyeon <bohyeonyoyo@gmail.com>
2024-09-20 09:05:46 +0900

    적 코드 구조

---
+++ Assets/02.Scripts/04.Enemy/Police.cs
@@ -0,0 +1,56 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : EnemyBase
+{
+    [BoxGroup("°æÂû"), LabelText("°ø°Ý·Â"), SerializeField, Range(1f, 20f)]
+    private float attackDamage = 10f;
+
+    [BoxGroup("°æÂû"), LabelText("°ø°Ý ¼Óµµ"), SerializeField, Range(1f, 20f)]
+    private float attackSpeed = 2f;
+
+    [BoxGroup("°æÂû"), LabelText("°ø°Ý ¹üÀ§"), SerializeField, Range(1f, 20f)]
+    private float attackRange = 10f;
+
+    [BoxGroup("°æÂû"), LabelText("ÃÑ¾Ë ÇÁ¸®ÆÕ"), SerializeField]
+    private GameObject bulletPrefab;
+
+    [BoxGroup("°æÂû"), LabelText("ÃÑ¾Ë ¼Óµµ"), SerializeField, Range(1f, 20f)]
+    private float bulletSpeed = 10f;
+
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void enemyAction()
+    {
+        if (eatAbleObjectBase.GetEaten())
+        {
+            return;
+        }
+
+        agent.SetDestination(targetPos());
+
+        float distanceToPlayer = Vector3.Distance(transform.position, targetPos());
+
+        if (distanceToPlayer < attackDamage)
+        {
+            agent.isStopped = true;
+
+        }
+        else if(distanceToPlayer >= attackSpeed) 
+        {
+            agent.isStopped = false;
+        
+        }
+    }
+}
