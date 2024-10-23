c42dd422bc3e316043e9ba3707e5f45e06a53607
Lee Bohyeon <bohyeonyoyo@gmail.com>
2024-10-23 13:24:17 +0900

    사운드 매니저 제작
    사운드 매니저 제작해서 사운드만 넣으면 작동될 예정입니다.

--- Assets/02.Scripts/01.Manager/OptionManager.cs
+++ Assets/02.Scripts/01.Manager/OptionManager.cs
@@ -8,13 +8,22 @@ public class OptionManager : MonoBehaviour
 {
     public static OptionManager Instance { get; private set; } // 싱글턴 선언
 
-    public GameObject option;
-    public GameObject optionUI;
-    public GameObject exitBtn;
+    [SerializeField] private GameObject option;
+    [SerializeField] private GameObject optionUI;
+    [SerializeField] private GameObject exitBtn;
+
+    [SerializeField] private Slider[] soundSliders;
+    [SerializeField] private Button[] bgmButtons;
+    [SerializeField] private Button[] sfxButtons;
 
     private Vector2 optionOriginalPos;
     private Vector2 btnOriginalPos;
 
+    private bool isBgmMuted = false;
+    private bool isSfxMuted = false;
+
+    private float previousBgmVolume = 1f;
+    private float previousSfxVolume = 1f;
 
     private void Awake()
     {
@@ -25,7 +34,7 @@ public class OptionManager : MonoBehaviour
             // RectTransform에서 처음 위치를 가져옴
             optionOriginalPos = optionUI.GetComponent<RectTransform>().anchoredPosition;
             btnOriginalPos = exitBtn.GetComponent<RectTransform>().anchoredPosition;
-            
+
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -33,6 +42,20 @@ public class OptionManager : MonoBehaviour
             DestroyImmediate(gameObject);
         }
 
+        LoadSoundSettings();
+    }
+
+    private void Start()
+    {
+        // BGM 버튼 이벤트 등록
+        bgmButtons[0].onClick.AddListener(() => SetBgmVolume(0)); // 볼륨 증가
+        bgmButtons[1].onClick.AddListener(() => SetBgmVolume(1)); // 볼륨 감소
+        bgmButtons[2].onClick.AddListener(() => ToggleBgmMute()); // 음소거 토글
+
+        // SFX 버튼 이벤트 등록
+        sfxButtons[0].onClick.AddListener(() => SetSfxVolume(0)); // 볼륨 증가
+        sfxButtons[1].onClick.AddListener(() => SetSfxVolume(1)); // 볼륨 감소
+        sfxButtons[2].onClick.AddListener(() => ToggleSfxMute()); // 음소거 토글
 
     }
 
@@ -62,15 +85,15 @@ public class OptionManager : MonoBehaviour
                 optionRect.anchoredPosition = optionOriginalPos;
                 btnRect.anchoredPosition = btnOriginalPos;
 
-                
+
 
                 option.SetActive(false); // 옵션 UI 끄기
-                
+
                 if (GameManager.Instance)
                 {
                     GameManager.Instance.SetOption();
                 }
-                
+
             });
 
             // 타임 스케일에 상관없이 애니메이션이 작동하도록 설정
@@ -105,5 +128,105 @@ public class OptionManager : MonoBehaviour
 
             enterOptionSequence.SetUpdate(true);
         }
+    }
+
+    private void SetBgmVolume(int index)
+    {
+        // 음소거 상태 해제
+        if (isBgmMuted)
+        {
+            isBgmMuted = false;           
+        }
+
+        float volumeChange = 0.1f;
+        if (index == 1)
+        {
+            soundSliders[0].value = Mathf.Max(0, soundSliders[0].value - volumeChange);
+        }
+        else
+        {
+            soundSliders[0].value = Mathf.Min(1, soundSliders[0].value + volumeChange);
+        }
+
+        AudioManager.Instance.SetBgmVolume(soundSliders[0].value);
+    }
+
+
+    private void SetSfxVolume(int index)
+    {
+        // 음소거 상태 해제
+        if (isSfxMuted)
+        {
+            isSfxMuted = false;
+        }
+
+        float volumeChange = 0.1f;
+        if (index == 1)
+        {
+            soundSliders[1].value = Mathf.Max(0, soundSliders[1].value - volumeChange);
+        }
+        else
+        {
+            soundSliders[1].value = Mathf.Min(1, soundSliders[1].value + volumeChange);
+        }
+
+        AudioManager.Instance.SetSfxVolume(soundSliders[1].value);
+    }
+
+    private void ToggleBgmMute()
+    {
+        isBgmMuted = !isBgmMuted;
+
+        AudioManager.Instance.MuteBGM(isBgmMuted);
+
+        // 사운드 설정 저장
+        SaveSoundSettings();
+    }
+
+    private void ToggleSfxMute()
+    {
+        isSfxMuted = !isSfxMuted;
+
+
+        AudioManager.Instance.MuteSFX(isSfxMuted);
+
+        // 사운드 설정 저장
+        SaveSoundSettings();
+    }
+
+
+    private void LoadSoundSettings()
+    {
+        // 저장된 데이터 로드
+        GameData data = SaveManager.Instance.LoadPlayerData();
+
+        // 볼륨 및 음소거 상태 적용
+        soundSliders[0].value = data.bgmVolume;
+        soundSliders[1].value = data.sfxVolume;
+
+        isBgmMuted = data.isBgmMuted;
+        isSfxMuted = data.isSfxMuted;
+
+        // AudioManager에 적용
+        AudioManager.Instance.SetBgmVolume(data.bgmVolume);
+        AudioManager.Instance.SetSfxVolume(data.sfxVolume);
+
+        AudioManager.Instance.MuteBGM(isBgmMuted);
+        AudioManager.Instance.MuteSFX(isSfxMuted);
+    }
+
+    private void SaveSoundSettings()
+    {
+        // 현재 데이터 로드
+        GameData data = SaveManager.Instance.LoadPlayerData();
+
+        // 사운드 설정 업데이트
+        data.bgmVolume = soundSliders[0].value;
+        data.sfxVolume = soundSliders[1].value;
+        data.isBgmMuted = isBgmMuted;
+        data.isSfxMuted = isSfxMuted;
+
+        // 데이터 저장
+        SaveManager.Instance.SavePlayerData(data);
     }
 }
