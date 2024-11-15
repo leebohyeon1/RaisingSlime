6d3f5789841c6416b8773be27edb55049dba8833
fivetoo52 <fivetoo52@gmail.com>
2024-11-16 06:08:44 +0900

    프리팹, 맵 제작
    시민 프리팹 및 동물 프리펩 제작. 도시2 리뉴얼 및 평지 일부 수정

--- Assets/02.Scripts/10.Tutorial/TutorialCitizen.cs
+++ Assets/02.Scripts/10.Tutorial/TutorialCitizen.cs
@@ -3,64 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialCitizen : EatAbleObjectBase
+public class TutorialCitizen : MonoBehaviour
 {
     [SerializeField]
-    private Transform arrowObj;
+    private float jumpDuration = 0.5f;
 
     private bool isJump = false;
-    private bool isMove = false;
 
     private Vector3[] defaultTrans = new Vector3[2];
 
-    protected override void Start()
+    protected void Start()
     {
         defaultTrans[0] = transform.position;
-        defaultTrans[1] = arrowObj.position;
-
-        GameLogicManager.Instance.RegisterUpdatableObject(this);
     }
 
     private void Update()
     {
-        if (GetEaten())
-        {
-            return;
-        }
-
-        if(!isJump)
+        if (!isJump)
         {
             isJump = true;
             LoopJump();
         }
+}
 
-        if (!isMove)
-        {
-            isMove = true;
-            LoopArrow();
-        }
-    }
-
-    void LoopJump()
+void LoopJump()
     {
-        transform.DOMoveY(transform.position.y + 0.6f, 0.25f).SetEase(Ease.OutQuad)
-            .OnComplete(()=> { transform.DOMoveY(defaultTrans[0].y, 0.25f).SetEase(Ease.InQuad).
+        transform.DOMoveY(transform.position.y + 0.6f, jumpDuration).SetEase(Ease.OutQuad)
+            .OnComplete(()=> { transform.DOMoveY(defaultTrans[0].y, jumpDuration).SetEase(Ease.InQuad).
                 OnComplete(()=> { isJump = false; });  });
     }
-
-    void LoopArrow()
-    {
-        arrowObj.DOMoveY(arrowObj.position.y + 0.2f, 0.4f).SetEase(Ease.InOutSine)
-            .OnComplete(() => { arrowObj.DOMoveY(defaultTrans[1].y , 0.4f).SetEase(Ease.InOutSine)
-                .OnComplete(()=> { isMove = false; }); });
-    }
-
-    public override void Eaten(Transform slimeTrans)
-    {
-        base.Eaten(slimeTrans);
-
-        Destroy(arrowObj.gameObject);
-    }
-
-
 }
