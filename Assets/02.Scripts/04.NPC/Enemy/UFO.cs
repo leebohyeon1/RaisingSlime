89e623a5c03d4b8324c5bade4d7704c780ea0018
Lee Bohyeon <42289387+leebohyeon1@users.noreply.github.com>
2024-11-20 01:49:03 +0900

    에셋 수정

--- Assets/02.Scripts/04.NPC/Enemy/UFO.cs
+++ Assets/02.Scripts/04.NPC/Enemy/UFO.cs
@@ -1,3 +1,4 @@
+using Pathfinding.RVO;
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
@@ -17,6 +18,24 @@ public class UFO : NPCBase
     [BoxGroup("UFO"), LabelText("높이"), SerializeField]
     private float heightFactor = 2f; // 플레이어 크기에 비례한 높이 조정
 
+    [BoxGroup("UFO"), LabelText("발사 위치"), SerializeField]
+    private Transform firePos;
+
+    [BoxGroup("UFO"), LabelText("빔"), SerializeField]
+    private GameObject lineRendererPrefab;
+    [BoxGroup("UFO"), LabelText("빔 끝"), SerializeField]
+    private GameObject beamEndPrefab;
+
+
+    [Header("Adjustable Variables")]
+    public float beamEndOffset = 1f; //How far from the raycast hit point the end effect is positioned
+    public float textureScrollSpeed = 8f; //How fast the texture scrolls along the beam
+    public float textureLengthScale = 3; //Length of the beam texture
+
+    private GameObject beamEnd;
+    private GameObject beam;
+    private LineRenderer line;
+
     private Player player;
 
     protected override void Awake()
@@ -34,6 +53,9 @@ public class UFO : NPCBase
 
         player = target.GetComponent<Player>();
 
+        beamEnd = Instantiate(beamEndPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        beam = Instantiate(lineRendererPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        line = beam.GetComponent<LineRenderer>();
 
         GameLogicManager.Instance.RegisterUpdatableObject(this);
     }
@@ -70,17 +92,32 @@ public class UFO : NPCBase
     // 지속적으로 플레이어 공격
     private void Attack()
     {
+        ShootBeamInDir(firePos.position, target.position - firePos.position);
         float damage = damagePerSecond * Time.deltaTime;
 
         player.TakeDamage(damage);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        
-    }
-    private void OnTriggerExit(Collider other)
-    {
 
+    void ShootBeamInDir(Vector3 start, Vector3 dir)
+    {
+        line.positionCount = 2;
+        line.SetPosition(0, start);
+
+        Vector3 end = Vector3.zero;
+        RaycastHit hit;
+        if (Physics.Raycast(start, dir, out hit))
+            end = hit.point - (dir.normalized * beamEndOffset);
+        else
+            end = transform.position + (dir * 100);
+
+        beamEnd.transform.position = end;
+        line.SetPosition(1, end);
+
+        beamEnd.transform.LookAt(firePos.position);
+
+        float distance = Vector3.Distance(start, end);
+        line.sharedMaterial.mainTextureScale = new Vector2(distance / textureLengthScale, 1);
+        line.sharedMaterial.mainTextureOffset -= new Vector2(Time.deltaTime * textureScrollSpeed, 0);
     }
 }
